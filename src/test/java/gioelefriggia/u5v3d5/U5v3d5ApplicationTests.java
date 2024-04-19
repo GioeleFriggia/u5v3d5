d54bfef11041fef8befe5a86abe1292bc6061c7f
gioele <gioelefriggia@hotmail.com>
2024-04-19 09:34:58 +0100

    Inizio

---
+++ src/test/java/gioelefriggia/u5v3d5/U5v3d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package gioelefriggia.u5v3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5v3d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
